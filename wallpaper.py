834f4940bf17a3136b125052f2579ccad01662ed
Lucas Rodés-Guirao <lucasrg@kth.se>
2018-07-13 12:34:32 +0200

    modularise

--- wallpaper.py
+++ wallpaper.py
@@ -5,41 +5,92 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen, urlretrieve
 import datetime
 import json
-
-path = '/home/pol.naranjo/wallpaper/'
-pics_path = '/home/pol.naranjo/wallpaper/pics/'
-today = datetime.datetime.strftime(datetime.datetime.today(), '%Y-%m-%d')
-#if 'wallpaper_{}.jpg'.format(today) not in os.listdir(pics_path):
-url = 'https://www.nationalgeographic.com/photography/photo-of-the-day/'
-f = urlopen(url)
-soup = BeautifulSoup(f, 'lxml')
-url_image = soup.find("meta",  property="og:image")['content']
-urlretrieve(url_image, os.path.join(pics_path, 'wallpaper_{}.jpg'.format(today)))
-
-a = soup.find_all(class_ = 'ad-template-type--pod')[0]
-b = a.find_all('section', class_='main-section')[0]
-c = b.find_all('article', class_='news__article sticky-box-ad-area')[0]
-#d = c.find_all('div', id_='article__body__wrap')
-d = c.find_all('div', class_='article-controller lead')[0]
-e = d.find_all('div', class_='lead-component')[0]
-f = e.find_all('script')[0]
-text = f.get_text().rstrip().lstrip()
-text_json=json.loads(text)
-text = text_json['dek']['dek']['text'].replace('<p>','').replace('</p>','')
-text_file = open(os.path.join(path, 'wallpaper.txt'), "w")
-text_file.write(text)
-text_file.close()
-
-
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-img = Image.open(os.path.join(pics_path, 'wallpaper_{}.jpg'.format(today)))
-draw = ImageDraw.Draw(img)
-# font = ImageFont.truetype(<font-file>, <font-size>)
-#font = ImageFont.truetype("sans-serif.ttf", 16)
-font = ImageFont.truetype('/usr/share/fonts/gnu-free/FreeSerif.ttf', 48)
-# draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((0, 0),text,(0,0,0))#,font=font)
-img.save('sample-out.jpg')
+
+class NatGeoImageWallpaper:
+
+    def __init__(self, url_natgeo):
+        # Path of project
+        self.pwd = os.getcwd()
+        # Path to picture archive
+        self.pictures_path = os.path.join(self.pwd, 'pics')
+        self.assets_path = os.path.join(self.pwd, 'assets')
+        # URL Nat Geo Photo of the Day
+        self.url_natgeo = url_natgeo
+        # Today's date
+        self.today = datetime.datetime.strftime(datetime.datetime.today(),
+                                                '%Y-%m-%d')
+        self.soup = None
+
+    def soup(self):
+        """ Get web soup, i.e. xml and stuff.
+        """
+        f = urlopen(self.url_natgeo)
+        self.soup = BeautifulSoup(f, 'lxml')
+
+    def download_image(self):
+        """ Downloads the NatGeo's Photo of the Day using the soup extracted
+            from the website. Stores the image under the folder 'pics' with name
+            `wallpaper_<today's date>.jpg`.
+        """
+        url_image = self.soup.find("meta", property="og:image")['content']
+        urlretrieve(url_image,
+                    os.path.join(self.pictures_path,
+                                 'wallpaper_{}.jpg'.format(self.today)))
+
+    def get_image_description(self):
+        # TODO: Work in progress
+        a = self.soup.find_all(class_='ad-template-type--pod')[0]
+        b = a.find_all('section', class_='main-section')[0]
+        c = b.find_all('article', class_='news__article sticky-box-ad-area')[0]
+        # d = c.find_all('div', id_='article__body__wrap')
+        d = c.find_all('div', class_='article-controller lead')[0]
+        e = d.find_all('div', class_='lead-component')[0]
+        f = e.find_all('script')[0]
+        text = f.get_text().rstrip().lstrip()
+        text_json = json.loads(text)
+        text = text_json['dek']['dek']['text'].replace('<p>', '').replace('</p>',
+                                                                          '')
+        text_file = open(os.path.join(self, 'wallpaper.txt'), "w")
+        text_file.write(text)
+        text_file.close()
+
+
+def insert_text_on_image(text, date, font=None):
+    # Load font
+    if not font:
+        font = '/usr/share/fonts/gnu-free/FreeSerif.ttf'
+    font = ImageFont.truetype(font, 48)
+
+    # Load image
+    img = Image.open(os.path.join(pics_path, 'wallpaper_{}.jpg'.format(date)))
+    draw = ImageDraw.Draw(img)
+
+    # font = ImageFont.truetype(<font-file>, <font-size>)
+    #font = ImageFont.truetype("sans-serif.ttf", 16)
+    # draw.text((x, y),"Sample Text",(r,g,b))
+
+    # Insert text on image
+    draw.text((0, 0), text, (0, 0, 0))#,font=font)
+
+    # Generate new image with text
+    img.save('wallpaper.jpg')
+
+
+if __name__ == "main":
+    # URL to NatGeo's photo of the day
+    url_natgeo = \
+        'https://www.nationalgeographic.com/photography/photo-of-the-day/'
+    # Declare instance
+    ngiw = NatGeoImageWallpaper(url_natgeo=url_natgeo)
+    ngiw.soup()
+
+    soup = get_web_soup(url_nat_geo)
+    download_image(soup)
+    get_image_description(soup)
+    insert_text_on_image("w", today)
+
+#if 'wallpaper_{}.jpg'.format(today) not in os.listdir(pics_path):
